055f7dea9c5df551f4cc44099a948479ffb8e128
Jack Xu <yurusanP@gmail.com>
2023-02-18 21:09:28 +0000

    cf1793: a+

---
+++ src/d02/cf1793/a/sol-opt.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+template <typename T>
+pair<T, T> qr(T a, T b) {
+  T q = a / b;
+  T r = a % b;
+  return {q, r};
+}
+
+void sol() {
+  ll a, b, n, m;
+  cin >> a >> b >> n >> m;
+  
+  // q: number of promotions
+  // r: remaining potatoes
+  auto [q, r] = qr(n, m + 1);
+  
+  ll ans = q * min(a * m, b * (m + 1)) + r * min(a, b);
+  cout << ans << "\n";
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    sol();
+  }
+}
---
+++ src/d02/cf1793/notes.md
@@ -0,0 +1,11 @@
+# [src.cp.cf1793] Codeforces Round #852 (Div. 2)
+
+[toc]
+
+---
+
+## (a) Yet Another Promotion
+
+Trivial.
+
+Avoid using `double` if possible. It seems that the server has a different precision from the one on my local machine.
