9e52a55df84187d21422b9b7942eabfa42fd8198
Jack Xu <yurusanP@gmail.com>
2023-02-21 23:57:24 +0000

    template: why don't I keep solve()

--- src/template/sanpo/sol.cpp
+++ src/template/sanpo/sol.cpp
@@ -12,7 +12,15 @@ typedef vector<int> vi;
 typedef pair<ll, ll> pll;
 typedef vector<ll> vl;
 
+void solve() {}
+
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
+
+  // int t;
+  // cin >> t;
+  // while (t--) {
+  //   solve();
+  // }
 }
