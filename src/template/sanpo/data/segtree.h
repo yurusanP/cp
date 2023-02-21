70b8382dc7c618b6fcf629ed61174618cd767b3e
Jack Xu <yurusanP@gmail.com>
2023-02-21 16:50:16 +0000

    template: lazy segtree

--- src/template/sanpo/data/segtree.h
+++ src/template/sanpo/data/segtree.h
@@ -2,88 +2,114 @@
 #define SANPO_DATA_SEGTREE_H_
 #include "../sol.h"
 
-// set point, query range
-template <typename T, typename F = function<T(const T&, const T&)>>
-struct zkw {
-  F fn;
-  T unit;
-  int n;
-  // [1, n) segments of fn results
-  // [n, 2n) points
-  vector<T> fs;
+struct segment {
+  static const int inf = 1e9;
+  static const int unit = -inf, unit_set = inf, unit_update = 0;
+  static int fn(int a, int b) { return max(a, b); }
 
-  zkw(const F& fn, T unit, int n) : fn(fn), unit(unit), n(n), fs(n * 2, unit) {}
-  zkw(const F& fn, T unit, vector<T>& es) : fn(fn), unit(unit), n(sz(es)), fs(n * 2, unit) {
-    rep(i, 0, n) fs[i + n] = es[i];
-    rrep(i, 1, n) fs[i] = fn(fs[i * 2], fs[i * 2 + 1]);
+  int lazy_set, lazy_update;
+  bool has_lazy_set() { return lazy_set != unit_set; }
+  bool has_lazy_update() { return lazy_update != unit_update; }
+  void apply_set(int x) {
+    val = x;
+    lazy_set = x;
+    lazy_update = unit_update;
+  }
+  void apply_update(int x) {
+    val += x;
+    if (has_lazy_set()) {
+      lazy_set += x;
+    } else {
+      lazy_update += x;
+    }
   }
 
-  // set point p
-  void set(int p, T x) {
-    p += n;
-    fs[p] = x;
-    for (; p /= 2;) {
-      fs[p] = fn(fs[p * 2], fs[p * 2 + 1]);
-    }
+  int val;
+  int l, r;
+  segment(int val, int l, int r) : val(val), l(l), r(r) {}
+  int len() const { return r - l; }
+};
+
+struct segtree {
+  using T = int;
+  vector<segment> fs;
+
+  segtree(vector<T>& es) {
+    auto build = [&](auto self, int ix, int l, int r) -> void {
+      if (r - l == 1) {
+        fs[ix] = segment(es[l], l, r);
+        return;
+      }
+
+      int mid = (l + r) / 2;
+      self(self, ix * 2, l, mid);
+      self(self, ix * 2 + 1, mid, r);
+
+      fs[ix] = segment(segment::fn(fs[ix * 2].val, fs[ix * 2 + 1].val), l, r);
+    };
+
+    fs.resize(sz(es) * 4);
+    build(build, 1, 0, sz(es));
   }
 
   // query range [l, r)
   T query(int l, int r) {
-    T res_l = unit, res_r = unit;
-    l += n;
-    r += n;
-    for (; l < r; l /= 2, r /= 2) {
-      if (l % 2) res_l = fn(res_l, fs[l++]);
-      if (r % 2) res_r = fn(fs[--r], res_r);
-    }
-    return fn(res_l, res_r);
+    auto query_aux = [&](auto self, int ix, int l, int r) -> T {
+      if (r <= fs[ix].l || fs[ix].r <= l) return segment::unit;
+      if (l <= fs[ix].l && fs[ix].r <= r) return fs[ix].val;
+
+      push(ix);
+
+      return segment::fn(self(self, ix * 2, l, r), self(self, ix * 2 + 1, l, r));
+    };
+    return query_aux(query_aux, 1, l, r);
   }
-};
 
-// update range, query point
-// assume that fn is commutative
-template <typename T>
-struct zkw_update {
-  int n;
-  // [1, n) segments of update tags
-  // [n, 2n) points
-  vector<T> fs;
+  // set range [l, r)
+  void set(int l, int r, T x) {
+    auto set_aux = [&](auto self, int ix, int l, int r, T x) -> void {
+      if (r <= fs[ix].l || fs[ix].r <= l) return;
+      if (l <= fs[ix].l && fs[ix].r <= r) {
+        fs[ix].apply_set(x);
+        return;
+      }
 
-  static T fn(const T& a, const T& b) { return a + b; }
-  static const T unit = 0;
+      push(ix);
+      self(self, ix * 2, l, r, x);
+      self(self, ix * 2 + 1, l, r, x);
 
-  zkw_update(int n) : n(n), fs(n * 2, unit) {}
-  zkw_update(vector<T>& es) : n(sz(es)), fs(n * 2, unit) {
-    rep(i, 0, n) fs[i + n] = es[i];
-    // [1, n) are initially units
+      fs[ix].val = segment::fn(fs[ix * 2].val, fs[ix * 2 + 1].val);
+    };
+    set_aux(set_aux, 1, l, r, x);
   }
 
   // update range [l, r)
   void update(int l, int r, T x) {
-    l += n;
-    r += n;
-    for (; l < r; l /= 2, r /= 2) {
-      if (l % 2) fn(fs[l], x), l++;
-      if (r % 2) --r, fn(fs[r], x);
-    }
+    auto update_aux = [&](auto self, int ix, int l, int r, T x) -> void {
+      if (r <= fs[ix].l || fs[ix].r <= l) return;
+      if (l <= fs[ix].l && fs[ix].r <= r) {
+        fs[ix].apply_update(x);
+        return;
+      }
+
+      push(ix);
+      self(self, ix * 2, l, r, x);
+      self(self, ix * 2 + 1, l, r, x);
+
+      fs[ix].val = segment::fn(fs[ix * 2].val, fs[ix * 2 + 1].val);
+    };
+    update_aux(update_aux, 1, l, r, x);
   }
 
-  // query point p
-  T query(int p) {
-    T res = unit;
-    p += n;
-    for (; p /= 2;) {
-      res = fn(res, fs[p]);
-    }
-    return res;
-  }
-
-  // push all update tags on segments to points
-  void push() {
-    rep(i, 1, n) {
-      fs[i * 2] = fn(fs[i * 2], fs[i]);
-      fs[i * 2 + 1] = fn(fs[i * 2 + 1], fs[i]);
-      fs[i] = unit;
+  void push(int ix) {
+    if (fs[ix].has_lazy_set()) {
+      fs[ix * 2].apply_set(fs[ix].lazy_set);
+      fs[ix * 2 + 1].apply_set(fs[ix].lazy_set);
+      fs[ix].lazy_set = segment::unit_set;
+    } else if (fs[ix].has_lazy_update()) {
+      fs[ix * 2].apply_update(fs[ix].lazy_update);
+      fs[ix * 2 + 1].apply_update(fs[ix].lazy_update);
+      fs[ix].lazy_update = segment::unit_update;
     }
   }
 };
